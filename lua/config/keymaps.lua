780130c943b21b165bd2cf329ac80c1f7a987967
chang <changpeng94@gmail.com>
2024-11-22 16:56:55 +0800

    fea: add outline plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,7 +4,7 @@
 
 local map = LazyVim.safe_keymap_set
 
-map("n",",c","<cmd>HopChar1<cr>")
+map("n",",c","<cmd>HopChar2<cr>")
 map("n",",s","<cmd>HopPattern<cr>")
 map("n",",l","<cmd>HopLineStart<cr>")
 map("n",",w","<cmd>HopWord<cr>")
