4cbd3a59c20123b4c703651fe8238ed01399f0b4
chang <changpeng94@gmail.com>
2024-11-26 12:13:00 +0800

    fea: add light theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,10 @@
 return {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = { transparent_background = true } },
+  { 'projekt0n/github-nvim-theme', name = 'github-theme' },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "github_light",
     },
   },
 }
